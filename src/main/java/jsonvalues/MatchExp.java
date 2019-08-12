c6ec887e705ad1f7827d978d3370812773656c85
rmerino <rmerino@paradigmadigital.com>
2019-08-13 01:15:46 +0200

    docs(javadoc): some methods in MatchExp were missing the javadoc
    re #15

--- src/main/java/jsonvalues/MatchExp.java
+++ src/main/java/jsonvalues/MatchExp.java
@@ -22,6 +22,7 @@ public class MatchExp
      @param ifValue the consumer to be invoked if this JsElem is a JsValue
      @param ifObj the consumer to be invoked if this JsElem is a JsObj
      @param ifArray the consumer to be invoked if this JsElem is a JsArray
+     @return consumer of a json element
      */
     public static Consumer<JsElem> accept(final Consumer<JsElem> ifValue,
                                           final Consumer<JsObj> ifObj,
@@ -41,11 +42,11 @@ public class MatchExp
     }
 
     /**
-     Declarative way of implementing if(this.isArray()) return ifArr.apply(this.asJsArray()) else return ifNotArr.apply(this)
+     return a matching expression to extract arrays out of json elements.
      @param ifArr the function to be applied if this JsElem is a JsArray
      @param ifNotArr the function to be applied if this JsElem is not a JsArray
      @param <T> the type of the object returned
-     @return an object of type T
+     @return a function that takes a JsElem and returns an object of type T
      */
     public static <T> Function<JsElem, T> ifArrElse(final Function<? super JsArray, T> ifArr,
                                                     final Function<? super JsElem, T> ifNotArr
@@ -57,11 +58,11 @@ public class MatchExp
     }
 
     /**
-     Declarative way of implementing if(this.isBool()) return ifBoolean.get() else ifNotBoolean.get()
+     return a matching expression to extract booleans out of json elements.
      @param ifBoolean the function to be applied if this JsElem is a JsBool
      @param ifNotBoolean the function to be applied if this JsElem is not a JsBool
      @param <T> the type of the object returned
-     @return an object of type T
+     @return a function that takes a JsElem and returns an object of type T
      */
     public static <T> Function<JsElem, T> ifBoolElse(final Function<? super Boolean, T> ifBoolean,
                                                      final Function<? super JsElem, T> ifNotBoolean
@@ -71,12 +72,12 @@ public class MatchExp
     }
 
     /**
-     Declarative way of returning an object based on the type of decimal number this element is
+     return a matching expression to extract decimal numbers out of json elements.
      @param ifDouble the function to be applied if this JsElem is a JsDouble
      @param ifBigDecimal the function to be applied if this JsElem is a JsBigDec
      @param ifOther the function to be applied if this JsElem is a not a decimal JsNumber
      @param <T> the type of the object returned
-     @return an object of type T
+     @return a function that takes a JsElem and returns an object of type T
      */
     public static <T> Function<JsElem, T> ifDecimalElse(final DoubleFunction<T> ifDouble,
                                                         final Function<BigDecimal, T> ifBigDecimal,
@@ -92,6 +93,15 @@ public class MatchExp
 
     }
 
+    /**
+     return a matching expression to extract integral numbers out of json elements.
+     @param ifInt function to be applied if the JsElem is a JsInt
+     @param ifLong function to be applied if the JsElem is a JsLong
+     @param ifBigInt function to be applied if the JsElem is a JsBigInt
+     @param ifOther function to be applied if the JsElem is a not an integral number
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifIntegralElse(final IntFunction<T> ifInt,
                                                          final LongFunction<T> ifLong,
                                                          final Function<BigInteger, T> ifBigInt,
@@ -108,6 +118,14 @@ public class MatchExp
 
     }
 
+    /**
+     return a matching expression to extract objs and arrays out of json elements.
+     @param ifObj function to be applied if the JsElem is a JsObj
+     @param ifArr function to be applied if the JsElem is not a JsArr
+     @param ifValue function to be applied if the JsElem is not a Json
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifJsonElse(final Function<? super JsObj, T> ifObj,
                                                      final Function<? super JsArray, T> ifArr,
                                                      final Function<? super JsElem, T> ifValue
@@ -124,6 +142,13 @@ public class MatchExp
         };
     }
 
+    /**
+     return a matching expression to extract jsons out of json elements.
+     @param ifJson function to be applied if the JsElem is a Json
+     @param ifNotJson function to be applied if the JsElem is not a Json
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifJsonElse(final Function<Json<?>, T> ifJson,
                                                      final Function<JsElem, T> ifNotJson
                                                     )
@@ -132,6 +157,13 @@ public class MatchExp
         return elem -> requireNonNull(elem).isJson() ? requireNonNull(ifJson).apply(elem.asJson()) : requireNonNull(ifNotJson).apply(elem);
     }
 
+    /**
+     return a matching expression to extract JsNothing out of json elements.
+     @param nothingSupplier supplier to be invoked if the JsElem is JsNothing
+     @param elseFn function to be applied if the JsElem is not JsNothing
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifNothingElse(final Supplier<T> nothingSupplier,
                                                         final Function<JsElem, T> elseFn
                                                        )
@@ -140,6 +172,13 @@ public class MatchExp
         return elem -> elem.isNothing() ? requireNonNull(nothingSupplier).get() : requireNonNull(elseFn).apply(elem);
     }
 
+    /**
+     return a matching expression to extract json objects out of json elements.
+     @param ifObj function to be applied if the JsElem is a JsObj
+     @param ifNotObj function to be applied if the JsElem is not a JsObj
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifObjElse(final Function<? super JsObj, T> ifObj,
                                                     final Function<? super JsElem, T> ifNotObj
                                                    )
@@ -151,6 +190,14 @@ public class MatchExp
         };
     }
 
+    /**
+     declarative way of implementing an if-else using high order functions
+     @param predicate the condition that will be tested on the json element
+     @param ifTrue the function to be applied if the predicate is evaluated to true
+     @param ifFalse the function to be applied if the predicate is evaluated to false
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifPredicateElse(final Predicate<JsElem> predicate,
                                                           final Function<JsElem, T> ifTrue,
                                                           final Function<JsElem, T> ifFalse
@@ -164,6 +211,13 @@ public class MatchExp
         };
     }
 
+    /**
+     returns a matching expression to extract strings out of json elements.
+     @param ifStr the function to be applied if the JsElem is a JsStr
+     @param ifNotStr the function to be applied if the JsElem is not a JsStr
+     @param <T> the type of the result
+     @return a function that takes a JsElem and returns an object of type T
+     */
     public static <T> Function<JsElem, T> ifStrElse(final Function<? super String, T> ifStr,
                                                     final Function<? super JsElem, T> ifNotStr
                                                    )
@@ -171,9 +225,4 @@ public class MatchExp
         return elem -> elem.isStr() ? requireNonNull(ifStr).apply(elem.asJsStr().x) : requireNonNull(ifNotStr).apply(elem);
     }
 
-    public static Predicate<JsElem> isSameType(final JsElem that)
-    {
-        return e -> e.getClass() == requireNonNull(that).getClass();
-
-    }
 }

134536987bd092c4278ba1af8628f9e66bd108bd
imrafaelmerino <rmerino@paradigmadigital.com>
2022-06-14 21:01:18 +0200

    performance

--- benchmarking/src/main/scripts/benchamark.sh
+++ benchmarking/src/main/scripts/benchamark.sh
@@ -1,86 +1,16 @@
 #!/usr/bin/env bash
-VERSION=11.1.1
+VERSION=11.3.0
+NOW=$(date +"%m-%d-%Y-%T" | tr ':' '_')
+
 java -Xms256m \
--jar releases/${VERSION}/benchmark-${VERSION}-zulu-8.jar \
--rff results/${VERSION}/zulu/deserializer/zulu-r8-c8.json \
+-jar releases/json-values-benchmark-${VERSION}.jar \
+-rff results/deserializers-${VERSION}-${NOW}.json \
 -rf json \
 jsonvalues\.benchmark\.JsDeserializers
 
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-8.jar \
-#-rff results/${VERSION}/open/serializer/open-r9-c8.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-9.jar \
-#-rff results/${VERSION}/open/deserializer/open-r9-c9.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-9.jar \
-#-rff results/${VERSION}/open/serializer/open-r9-c9.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-10.jar \
-#-rff results/${VERSION}/open/deserializer/open-r11-c10.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-10.jar \
-#-rff results/${VERSION}/open/serializer/open-r11-c10.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-11.jar \
-#-rff results/${VERSION}/open/deserializer/open-r11-c11.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-11.jar \
-#-rff results/${VERSION}/open/serializer/open-r11-c11.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-12.jar \
-#-rff results/${VERSION}/open/deserializer/open-r11-c12.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-12.jar \
-#-rff results/${VERSION}/open/serializer/open-r11-c12.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-13.jar \
-#-rff results/${VERSION}/open/deserializer/open-r11-c13.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-13.jar \
-#-rff results/${VERSION}/open/serializer/open-r11-c13.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-14.jar \
-#-rff results/${VERSION}/open/deserializer/open-r11-c14.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsDeserializers
-#
-#java -Xms256m \
-#-jar releases/${VERSION}/benchmark-${VERSION}-open-14.jar \
-#-rff results/${VERSION}/open/serializer/open-r11-c14.json \
-#-rf json \
-#jsonvalues\.benchmark\.JsSerializers
+java -Xms256m \
+-jar releases/json-values-benchmark-${VERSION}.jar \
+-rff results/serializers-${VERSION}-${NOW}.json \
+-rf json \
+jsonvalues\.benchmark\.JsSerializers
 

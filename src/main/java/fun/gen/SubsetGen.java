dae7615b41a30e064169b8fa3dfaf59a19472082
Rafael Merino García <imrafaelmerino@gmail.com>
2023-09-26 09:30:58 +0200

    new methods RecordGen, new shuffle gen, subsets generator more eficient

--- src/main/java/fun/gen/SubsetGen.java
+++ src/main/java/fun/gen/SubsetGen.java
@@ -4,13 +4,13 @@ import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
- class SubsetSupplier<O> implements Supplier<Set<O>> {
+class SubsetSupplier<O> implements Supplier<Set<O>> {
 
     private final Supplier<Stream<Set<O>>> s;
 
     private final List<O> inputs;
     private final Supplier<Long> gen;
-
+    private final Map<Integer, List<Set<O>>> memoizationMap;
 
     SubsetSupplier(List<O> inputs,
                    Random random) {
@@ -20,20 +20,12 @@ import java.util.stream.Stream;
                                  inputs.size()) - 1;
         this.gen = LongGen.arbitrary(0,
                                      i).apply(Objects.requireNonNull(random));
+        this.memoizationMap = new HashMap<>();
     }
 
     public SubsetSupplier(List<O> inputs) {
         this(inputs,
              new Random());
-    }
-
-    private static <I> Stream<Set<I>> subsets(List<I> elements) {
-        List<Set<I>> combinations = new ArrayList<>();
-        subsets(elements,
-                0,
-                new HashSet<>(),
-                combinations);
-        return combinations.stream();
     }
 
     private static <I> void subsets(List<I> elements,
@@ -56,6 +48,24 @@ import java.util.stream.Stream;
                 currentIndex + 1,
                 currentCombination,
                 combinations);
+    }
+
+    private Stream<Set<O>> subsets(List<O> elements) {
+        int hashCode = elements.hashCode();
+        if (memoizationMap.containsKey(hashCode)) {
+            return memoizationMap.get(hashCode).stream();
+        }
+
+        List<Set<O>> combinations = new ArrayList<>();
+        subsets(elements,
+                0,
+                new HashSet<>(),
+                combinations);
+        Stream<Set<O>> resultStream = combinations.stream();
+        memoizationMap.put(hashCode,
+                           combinations);
+
+        return resultStream;
     }
 
     Gen<Set<O>> gen() {
